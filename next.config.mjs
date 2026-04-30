7fd217b486a378a043b8c346c47f5df830dc44cc
Ali Mohammadnia <alimohammadnia127@gmail.com>
2026-04-30 23:58:02 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
+// import createNextIntlPlugin from 'next-intl/plugin';
+
+// const withNextIntl = createNextIntlPlugin('./i18n.ts');
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// export default withNextIntl(nextConfig)
+
+
 /** @type {import('next').NextConfig} */
 const isExport = process.env.NEXT_EXPORT === 'true'
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig = {
   output: isExport ? 'export' : 'standalone',
